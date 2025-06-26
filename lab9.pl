110b605d0d5415f987972db3962ae327435322e4
Meg-Lander <lander.mega2003mail@mail.ru>
2025-06-26 20:03:53 +0300

    Prolog LB9.3 complex relationship

--- lab9.pl
+++ lab9.pl
@@ -39,3 +39,26 @@ sisters(X) :- sister(Sister, X), write('Сестра '), write(X), write(': '), 
 
 b_s(X, Y) :- sibling(X, Y).
 b_s(X) :- b_s(Bs, X), write('Сиблинг '), write(X), write(': '), write(Bs), nl, fail. b_s(_).
+
+
+% 3 : Более сложные отношения
+
+% grand_pa(X, Y): X является дедушкой Y
+grand_pa_facts(X, Y) :- man(X), dite(Parent, Y), dite(X, Parent).
+grand_pa_preds(X, Y) :- man(X), (father(Parent, Y) ; mother(Parent, Y)), (father(X, Parent) ; mother(X, Parent)).
+
+grand_pas(X) :- write('Дедушки '), write(X), write(':'), nl, grand_pa_facts(GP, X), write(GP), nl, fail. grand_pas(_).
+
+% grand_son_of_pa(GS, GP): GS является внуком GP (по линии дедушки)
+grand_son_of_pa_facts(GS, GP) :- man(GS), grand_pa_facts(GP, GS).
+grand_son_of_pa_preds(GS, GP) :- man(GS), grand_pa_preds(GP, GS).
+
+% grand_pa_and_son(X, Y): X и Y дедушка и внук или наоборот
+grand_pa_and_son_facts(X, Y) :- (grand_pa_facts(X, Y), man(Y)); (grand_pa_facts(Y, X), man(X)).
+grand_pa_and_son_preds(X, Y) :- (grand_pa_preds(X, Y), man(Y)); (grand_pa_preds(Y, X), man(X)).
+
+% uncle(X, Y): X является дядей Y
+uncle_facts(X, Y) :- man(X), sibling(X, P), dite(P, Y).
+uncle_preds(X, Y) :- man(X), b_s(X, P), (father(P, Y) ; mother(P, Y)).
+
+uncles(X) :- write('Дядя '), write(X), write(': '), write(U), nl, fail. uncles(_).
