2482c3c8461e3171afdb6fd6d0e214d226800085
Meg-Lander <lander.mega2003mail@mail.ru>
2025-08-08 00:54:40 +0300

    Add file WorkWithDigits LB5.10

---
+++ labs_F#/Program.fs
@@ -0,0 +1,45 @@
+﻿[<EntryPoint>]
+     let main argv =
+         let number = 12345
+         
+         let result = WorkWithDigits.sum_digits_down number
+         printfn "Сумма цифр равна %d" result
+
+         let result = WorkWithDigits.sum_digits_top number
+         printfn "Сумма цифр равна %d" result
+
+         let result = WorkWithDigits.big_function (false)
+         System.Console.WriteLine (result (6))
+
+         let number = 123
+
+         let sum_function = fun a b -> a + b
+         let proiz_function = fun a b -> a * b
+         let min_function = fun a b -> if a < b then a else b
+         let max_function = fun a b -> if a > b then a else b
+     
+         let sumResult = WorkWithDigits.operation_on_digits_numbers (number) (sum_function) (10)
+         System.Console.WriteLine("Сумма цифр числа: {0}", sumResult)
+     
+         let proizResult = WorkWithDigits.operation_on_digits_numbers (number) (proiz_function) (1)
+         System.Console.WriteLine("Произведение цифр числа: {0}", proizResult)
+     
+         let minResult = WorkWithDigits.operation_on_digits_numbers (number) (min_function) (9)
+         System.Console.WriteLine("Минимальная цифра числа: {0}", minResult)
+     
+         let maxResult = WorkWithDigits.operation_on_digits_numbers (number) (max_function) (0)
+         System.Console.WriteLine("Максимальная цифра числа: {0}", maxResult)
+
+
+
+
+         let sumEvenDigits = operation_on_digits_numbers_with_condition (number) (sum_function) (0) (fun x -> x % 2 = 0)
+         System.Console.WriteLine("Сумма четных цифр числа: {0}", sumEvenDigits)
+
+         let proizDigits = operation_on_digits_numbers_with_condition (number) (proiz_function) (1) (fun x -> x > 5)
+         System.Console.WriteLine("Произведение цифр больше 5: {0}", proizDigits)
+
+         let minDigit = operation_on_digits_numbers_with_condition (number) (min_function) (9) (fun x -> x > 2)
+         System.Console.WriteLine("Минимальная цифра больше 2: {0}", minDigit)
+
+         0
---
+++ labs_F#/WorkWithDigits.fs
@@ -0,0 +1,56 @@
+module WorkWithDigits
+    let sum_digits_top number = 
+        let rec sum_digit number cur_sum =
+            if number = 0 then cur_sum
+            else 
+                let last_digit = number % 10
+                sum_digit (number / 10) (cur_sum + last_digit)
+        sum_digit number 0
+
+    let sum_digits_down number = 
+         let rec sum_digit number =
+             if number = 0 then 0
+             else 
+                 let last_digit = number % 10
+                 last_digit + sum_digit (number / 10)
+         sum_digit number
+
+    let factorial_top number =
+        let rec sum_bef number =
+            let proiz = number
+            if number = 0 then 1
+            else
+                proiz * sum_bef (number - 1)
+        sum_bef number
+
+    let factorial_down number =
+        let rec sum_bef number proiz =
+            if number = 0 then proiz
+            else
+                let last = number - 1
+                sum_bef (last) (number * proiz)
+        sum_bef number 1
+
+    let big_function flag =
+        match flag with
+            true -> sum_digits_top
+            | false -> factorial_top
+
+    let operation_on_digits_numbers number operation initialValue =
+        let rec oper num acc =
+            if num = 0 then acc
+            else
+                let lastDigit = num % 10
+                oper (num / 10) (operation acc lastDigit)
+        oper number initialValue
+
+    let operation_on_digits_numbers_with_condition number operation accum condition =
+        let rec oper num acc =
+            if num = 0 then acc
+            else
+                let lastDigit = num % 10
+                let newAcc = if condition lastDigit then operation acc lastDigit else acc
+                oper (num / 10) newAcc
+        oper number accum
+         
+    
